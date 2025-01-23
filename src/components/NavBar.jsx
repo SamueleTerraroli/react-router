d857b7ba30054eef815a7303d2b6c499383b0347
SamueleTerraroli <terraroli99@gmail.com>
2025-01-23 14:56:51 +0100

    feat: add root structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom"
 const NavBar = () => {
     return (
         <nav className="container d-flex justify-content-center p-4">
-            <NavLink className="navbar-brand m-4" to="/">Default</NavLink>
-            <NavLink className="navbar-brand m-4" to="/chi-siamo">Default</NavLink>
-            <NavLink className="navbar-brand m-4" to="/posts">Default</NavLink>
+            <NavLink className="navbar-brand m-4 text-light p-2 rounded-3 border" to="/">Home</NavLink>
+            <NavLink className="navbar-brand m-4 text-light p-2 rounded-3 border" to="/chi-siamo">Chi siamo</NavLink>
+            <NavLink className="navbar-brand m-4 text-light p-2 rounded-3 border" to="/posts">Post</NavLink>
         </nav>
     )
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -6,12 +6,16 @@ import NavBar from '../components/NavBar'
 const DefaultLayout = () => {
     return (
         <>
-            <header>
+            <header className='bg-dark'>
                 <NavBar />
             </header>
-            <main>
+            <main className='my-3 text-center'>
                 <Outlet />
             </main>
+            <footer className='bg-dark'>
+                <p className='text-light text-center p-3'>FOOTER</p>
+
+            </footer>
         </>
     )
 }
